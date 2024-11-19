f3701a611de276bf7e9f5a17aef61a269a4b1352
IvanPopme <votjavov5656@gmail.com>
2024-11-19 11:23:22 +0500

    Add files via upload

---
+++ Labs_PSTU_2024/pointers/2_pointer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    int* ptr1;
+    int* ptr2;
+    cin >> a >> b;
+    ptr1 = &a;
+    ptr2 = &b;
+    cout << *ptr1 + *ptr2 << endl;
+}
