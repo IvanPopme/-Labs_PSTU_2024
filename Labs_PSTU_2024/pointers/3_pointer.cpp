9d6ad0427ec8735b675e410dca07c315ee23e246
IvanPopme <votjavov5656@gmail.com>
2024-11-19 11:39:03 +0500

    Add files via upload

---
+++ Labs_PSTU_2024/pointers/3_pointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b;
+    float* ptr1;
+    float* ptr2;
+    cin >> a >> b;
+    ptr1 = &a;
+    ptr2 = &b;
+    cout << *ptr1 + *ptr2 << endl;
+    return 0;
+}
