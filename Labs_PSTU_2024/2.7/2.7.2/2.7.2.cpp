f9f535117b68956de0e27307a35352c91dd75cd5
IvanPopme <votjavov5656@gmail.com>
2024-11-25 23:05:45 +0500

    Add files via upload

---
+++ Labs_PSTU_2024/2.7/2.7.2/2.7.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n <= 1)
+	{
+		cout << "number > 1\n";
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
