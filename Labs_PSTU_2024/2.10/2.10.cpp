9f7dec332219bde92623b42a1ab8b96a25bff9be
IvanPopme <votjavov5656@gmail.com>
2024-12-05 09:31:15 +0500

    Add files via upload

---
+++ Labs_PSTU_2024/2.10/2.10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int n, tmp, max;
+    cout << "Введите длину последовательности:" << endl;
+    cin >> n;
+    cout << "Введите первое число" << endl;
+    cin >> max;
+    for (int i = 2; i <= n; i++)
+    {
+        cout << "Введите следующее число:" << endl;
+        cin >> tmp;
+        if (tmp > max)
+        {
+            max = tmp;
+        }
+    }
+    cout << "Максимальное число " << max << endl;
+    return 0;
+}
