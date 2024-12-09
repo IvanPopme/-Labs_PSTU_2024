ba0b85933f138740bd8ac2b9dd72b93b84031364
IvanPopme <votjavov5656@gmail.com>
2024-12-09 23:38:25 +0500

    Add files via upload

---
+++ Labs_PSTU_2024/2.22/2.22.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	float n, a, max;
+	int i = 2; // чтобы последовательность была приличная
+	int num = 1;
+	cin >> n;
+	max = sin(n + 1 / n);
+	while (i <= n)
+	{
+		a = sin(n + i / n);
+		if (a > max)
+		{
+			max = a;
+			num = i;
+		}
+		i++;
+	}
+	cout << "Максимальный элемент: " << max << " Его номер: " << num << endl;
+	return 0;
+}
