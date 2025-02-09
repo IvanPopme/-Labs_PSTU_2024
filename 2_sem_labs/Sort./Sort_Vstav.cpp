9403105bb4a63dae32b98249a5ceba1746d61299
IvanPopme <votjavov5656@gmail.com>
2025-02-09 20:21:33 +0500

    Create Sort_Vstav.cpp

---
+++ 2_sem_labs/Sort./Sort_Vstav.cpp
@@ -0,0 +1,36 @@
+ #include <iostream>
+ using namespace std;
+ int main()
+ {
+ int n;
+ cin >> n;
+ int arr[n];
+ int range_min, range_max;
+ range_min = 2;
+ range_max = 412;
+ for (int i = 0; i < n; i++)
+ {
+ arr[i] = ((double)rand() / RAND_MAX) * (range_max - 
+range_min) + range_min;
+ cout << arr[i] << " ";
+ }
+ cout << endl;
+ int key;
+ for (int i = 0; i < n; i++)
+ {
+ key = arr[i];
+ int j = i-1;
+ while (j >= 0 && arr[j] > key)
+ {
+ arr[j+1] = arr[j];
+ j--;
+ }
+ arr[j+1] = key;
+ }
+for (int i = 0; i < n; i++)
+ {
+ cout << arr[i] << " ";
+ }
+ cout << endl;
+ return 0;
+ }
