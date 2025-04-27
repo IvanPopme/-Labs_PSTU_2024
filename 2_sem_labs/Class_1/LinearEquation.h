9919cc2aeb569a85f5ecbcddc57c20a412de9584
IvanPopme <votjavov5656@gmail.com>
2025-04-27 22:25:07 +0500

    Create LinearEquation.h

---
+++ 2_sem_labs/Class_1/LinearEquation.h
@@ -0,0 +1,10 @@
+struct LinearEquation
+{
+    double A; // коэффициент A
+    double B; // коэффициент B
+
+    void Init(double a, double b);
+    void Read();
+    void Show();
+    double root();
+};
