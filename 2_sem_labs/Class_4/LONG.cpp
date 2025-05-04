cddc5ddd10123c96aabeae6aeb20a8304aabbb23
IvanPopme <votjavov5656@gmail.com>
2025-05-04 19:48:57 +0500

    Update LONG.cpp

--- 2_sem_labs/Class_4/LONG.cpp
+++ 2_sem_labs/Class_4/LONG.cpp
@@ -1,1 +1,30 @@
+#include "LONG.h"
 
+LONG::LONG() : PAIR() {}
+LONG::LONG(int high, int low) : PAIR(high, low) {}
+LONG::LONG(const LONG& l) : PAIR(l) {}
+
+LONG LONG::operator+(const LONG& l) const {
+    int new_high = first + l.first;
+    int new_low = second + l.second;
+    if (new_low >= 100) {
+        new_high += new_low / 100;
+        new_low %= 100;
+    }
+    return LONG(new_high, new_low);
+}
+
+LONG LONG::operator-(const LONG& l) const {
+    int new_high = first - l.first;
+    int new_low = second - l.second;
+    if (new_low < 0) {
+        new_high -= 1;
+        new_low += 100;
+    }
+    return LONG(new_high, new_low);
+}
+
+LONG LONG::operator*(const LONG& l) const {
+    int total = (first * 100 + second) * (l.first * 100 + l.second);
+    return LONG(total / 10000, (total / 100) % 100);
+}
