bfffcd61369f9ad2fb3acecc9063798904cdecda
IvanPopme <votjavov5656@gmail.com>
2025-05-04 19:48:31 +0500

    Update PAIR.h

--- 2_sem_labs/Class_4/PAIR.h
+++ 2_sem_labs/Class_4/PAIR.h
@@ -1,1 +1,25 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class PAIR {
+protected:
+    int first;
+    int second;
+
+public:
+    PAIR();
+    PAIR(int f, int s);
+    PAIR(const PAIR& p);
+
+    int GetFirst() const;
+    int GetSecond() const;
+    void SetFirst(int f);
+    void SetSecond(int s);
+
+    PAIR operator+(const PAIR& p) const;
+
+    friend std::istream& operator>>(std::istream& in, PAIR& p);
+    friend std::ostream& operator<<(std::ostream& out, const PAIR& p);
+};
 
