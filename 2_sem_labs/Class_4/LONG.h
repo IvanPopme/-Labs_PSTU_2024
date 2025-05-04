5f7df654ba2aeb91ea9117a38306f910b57ae654
IvanPopme <votjavov5656@gmail.com>
2025-05-04 19:49:14 +0500

    Update LONG.h

--- 2_sem_labs/Class_4/LONG.h
+++ 2_sem_labs/Class_4/LONG.h
@@ -1,1 +1,13 @@
+#pragma once
+#include "PAIR.h"
 
+class LONG : public PAIR {
+public:
+    LONG();
+    LONG(int high, int low);
+    LONG(const LONG& l);
+
+    LONG operator+(const LONG& l) const;
+    LONG operator-(const LONG& l) const;
+    LONG operator*(const LONG& l) const;
+};
