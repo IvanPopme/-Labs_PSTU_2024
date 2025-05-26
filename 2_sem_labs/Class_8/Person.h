d00ce9ccbcb1cc67beef4ff7476d0f262fbd5089
IvanPopme <votjavov5656@gmail.com>
2025-05-26 20:26:18 +0500

    Update Person.h

--- 2_sem_labs/Class_8/Person.h
+++ 2_sem_labs/Class_8/Person.h
@@ -1,19 +1,27 @@
 #pragma once
 #include "Object.h"
+#include "Event.h"
 
 class Person : public Object {
+public:
+    Person(void);
+    virtual ~Person(void);
+    void Show();
+    void Input();
+    void HandleEvent(const TEvent& e);
+    
+    Person(string name, int age);
+    Person(const Person& p);
+    
+    string Get_name() { return name; }
+    int Get_age() { return age; }
+    
+    void Set_name(string);
+    void Set_age(int);
+    
+    Person& operator=(const Person&);
+    
 protected:
     string name;
     int age;
-public:
-    Person();
-    virtual ~Person() {}
-    virtual void Show();
-    virtual void Input();
-    Person(string, int);
-    Person(const Person&);
-    void SetName(string);
-    void SetAge(int);
-    string GetName();
-    int GetAge();
 };
